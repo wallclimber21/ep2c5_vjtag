3b4c3a976aa5e54955bda9e57714060b880e6473
Tom Verbeure <tom_verbeure@yahoo.com>
2016-09-04 22:47:14 -0700

    Control LEDs from Python script

--- rtl/vjtag_client.v
+++ rtl/vjtag_client.v
@@ -2,15 +2,43 @@
 module vjtag_client(
 		input	wire		tck,
 		input	wire		tdi,
-		output	wire		tdo,
+		output	reg			tdo,
 		input	wire [1:0]	ir_in,
 		input	wire		virtual_state_sdr,
 		input	wire		virtual_state_udr,
-		output	wire [1:0]	leds
+		output	reg [1:0]	leds
 	);
 
-	assign tdo = tdi;
+	wire select_dr0		= (ir_in == 2'd0);
+	wire select_dr1		= (ir_in == 2'd1);
 
-	assign leds	= { 1'b1, 1'b0 };
+	reg			dr0_bypass;
+	reg [6:0]	dr1;
+
+	always @(posedge tck) 
+	begin
+		dr0_bypass <= tdi;
+
+		if (virtual_state_sdr) begin
+			if (select_dr1) begin
+				dr1 <= {tdi, dr1[6:1] };
+			end
+		end
+	end
+
+	always @*
+	begin
+		if (select_dr1) begin
+			tdo <= dr1[0];
+		end
+		else begin
+			tdo <= dr0_bypass;
+		end
+	end
+
+	always @(virtual_state_udr) 
+	begin
+		leds = { dr1[6], dr1[0] };
+	end
 
 endmodule
