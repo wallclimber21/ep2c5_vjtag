52c4df72771d143ec0b68ed55ea410e4dfc63baa
Tom Verbeure <tom_verbeure@yahoo.com>
2016-09-04 21:42:43 -0700

    Instantiate vjtag and empty vjtag_client

--- rtl/top.v
+++ rtl/top.v
@@ -21,8 +21,44 @@ module top(
 		end
 	end
 
-	assign led0 = ~counter[26];
-	assign led1 = ~counter[25];
 	assign led2 = ~counter[24];
+
+	wire		vjtag_m2s_tck;
+	wire		vjtag_m2s_tdi;
+	wire		vjtag_s2m_tdo;
+
+	wire [1:0] vjtag_m2s_ir;
+
+	wire		virtual_state_sdr;
+
+	vjtag u_vjtag (
+		.tck				( vjtag_m2s_tck ),
+		.tdi				( vjtag_m2s_tdi ),
+		.tdo				( vjtag_s2m_tdo ),
+
+		.ir_out				(				),
+		.ir_in				( vjtag_m2s_ir  ),
+
+		.virtual_state_cdr	( ),
+		.virtual_state_cir  ( ),
+		.virtual_state_e1dr	( ),
+		.virtual_state_e2dr	( ),
+		.virtual_state_pdr	( ),
+		.virtual_state_sdr	( virtual_state_sdr ),
+		.virtual_state_udr	( ),
+		.virtual_state_uir	( )
+		);
+
+	vjtag_client u_vjtag_client (
+		.tck				( vjtag_m2s_tck ),
+		.tdi				( vjtag_m2s_tdi ),
+		.tdo				( vjtag_s2m_tdo ),
+
+		.ir_in				( vjtag_m2s_ir  ),
+
+		.virtual_state_sdr	( virtual_state_sdr ),
+
+		.leds				( { led1, led0 } )
+	);
 
 endmodule
---
+++ rtl/vjtag_client.v
@@ -0,0 +1,15 @@
+
+module vjtag_client(
+		input	wire		tck,
+		input	wire		tdi,
+		output	wire		tdo,
+		input	wire [1:0]	ir_in,
+		input	wire		virtual_state_sdr,
+		output	wire [1:0]	leds
+	);
+
+	assign tdo = tdi;
+
+	assign leds	= { 1'b1, 1'b0 };
+
+endmodule
